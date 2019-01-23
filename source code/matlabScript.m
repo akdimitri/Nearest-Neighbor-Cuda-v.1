32bbc1cc50cea54f5912f7175de06a7b91f4c4af
akdimitri <44363271+akdimitri@users.noreply.github.com>
2019-01-23 19:27:49 +0200

    Add files via upload

---
+++ source code/matlabScript.m
@@ -0,0 +1,32 @@
+%Matlab Script Q,C Sets Generation
+%NN calculation
+
+N = 2^21
+C = rand(N,3);
+Q = rand(N,3);
+%Specify Indeces and shortest Distances
+[Idx, D] = knnsearch( C, Q);
+
+%Write Distances to files
+fileID = fopen('./test/MatlabDistances21.txt','w');
+fprintf( fileID,'%f\n', D);
+fclose(fileID);
+
+%Write Indeces to File
+fileID = fopen('./test/MatlabIndeces21.txt','w');
+fprintf( fileID,'%d\n', Idx);
+fclose(fileID);
+
+%Write Q to File
+fileID = fopen('./test/MatlabQ21.txt','w');
+for i = 1:N
+fprintf( fileID,'%f %f %f\n', Q(i,1), Q(i,2), Q(i,3));
+end
+fclose(fileID);
+
+%Write C to File
+fileID = fopen('./test/MatlabC21.txt','w');
+for i = 1:N
+fprintf( fileID,'%f %f %f\n', C(i,1), C(i,2), C(i,3));
+end
+fclose(fileID);
